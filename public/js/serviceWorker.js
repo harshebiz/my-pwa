fd4e881608f7689c310b83facc60bfb9fa3fee0e
Harsh Vekariya <harsh.vekariya@esparkbizmail.com>
2025-11-26 17:50:45 +0530

    commit

--- public/js/serviceWorker.js
+++ public/js/serviceWorker.js
@@ -1,21 +1,48 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
 var assets = [
     "/",
     "/index.html",
-    "/js/app.js"
+    "/js/app.js",
+    '/icons/icon_32x32.png',
+    '/icons/icon_128x128.png',
+    '/icons/icon_256x256.png',
+    '/icons/icon_512x512.png',
 ]
 
-self.addEventListener("install", function(installEvent) {
-    installEvent.waitUntil(
-        caches.open("my-test-pwa").then(function(cache) {
-            cache.addAll(assets)
-        })
-    )
-})
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
-self.addEventListener("fetch", function(fetchEvent) {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(function(res) {
-            return res || fetch(fetchEvent.request)
+// Activate event - delete old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
         })
-    )
-})
+      );
+    })
+  );
+});
+
+// Fetch event - intercept requests to serve cached content
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+  );
+});
