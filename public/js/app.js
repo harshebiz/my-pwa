44eabcf250f7d0bcc379055f496949a9022c48a8
Harsh Vekariya <harsh.vekariya@esparkbizmail.com>
2025-11-26 18:05:22 +0530

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ document.addEventListener("click", function() {
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
         navigator.serviceWorker
-            .register("/my-pwa/js/serviceWorker.js")
+            .register("https://harshebiz.github.io/my-pwa/js/serviceWorker.js")
             .then(function() {
                 console.log("service worker registered")
             })
